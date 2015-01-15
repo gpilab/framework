08d068bd27493f5856138321b5eb7e0f287e39fb
ash <aganders3@gmail.com>
2015-01-15 00:04:52 -0700

    Tests match those for Armadillo.

--- include/PyFI/eigen_test.py
+++ include/PyFI/eigen_test.py
@@ -1,11 +1,35 @@
 #!/opt/gpi/bin/python
-import eigen 
-import numpy as np
+"""A simple test script for the PyFI eigen wrapper.
+"""
 
-A = np.random.random((5,5)).astype(np.float32)
-B = np.zeros(A.shape[::-1]).astype(np.float32)
-print A
-eigen.pinv(A,B)
-print B
-print np.linalg.pinv(A)
+import numpy as np
+import eigen
+import timeit
+
+if __name__ == "__main__":
+    m = 50 
+    n = 50 
+    A = 1000*np.random.random((m,n)).astype(np.float32)
+    npAinv = np.linalg.pinv(A)
+    eigenAinv = eigen.pinv(A)
+    if np.allclose(npAinv, eigenAinv, rtol=0.01):
+        init = """import eigen, numpy as np
+m,n = 50,50
+A = 1000*np.random.random((m,n)).astype(np.float32)"""
+        a = timeit.timeit('np.linalg.pinv(A)', setup=init, number=1000)
+        b = timeit.timeit('eigen.pinv(A)', setup=init, number=1000)
+        print "eigen successfully inverted the {}x{} matrix {} times faster than numpy".format(m,n,a/b)
+
+    p = 3
+    B = 1000*np.random.random((m,p)).astype(np.float32)
+    eigenX = eigen.solve(A,B)
+    npX = np.linalg.lstsq(A,B)[0]
+    if np.allclose(eigenX, npX, rtol=0.01):
+        init = """import eigen as eigen, numpy as np
+m,n,p = 50,50,3
+A = 1000*np.random.random((m,n)).astype(np.float32)
+B = 1000*np.random.random((m,p)).astype(np.float32)"""
+        a = timeit.timeit('np.linalg.lstsq(A,B)', setup=init, number=1000)
+        b = timeit.timeit('eigen.solve(A,B)', setup=init, number=1000)
+        print "eigen successfully solved the equatioon AX=B {} times faster than numpy\n\twhere A was {}x{}, and B was {}x{}".format(a/b,m,n,m,p)
 
