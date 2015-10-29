118c7a19fb09e0aafe379ac753cbc1d5a86788a9
nick <nick.zwart@gmail.com>
2015-10-28 19:34:13 -0700

    A simple window.

---
+++ lib/gpi/update.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#    Copyright (C) 2014  Dignity Health
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    NO CLINICAL USE.  THE SOFTWARE IS NOT INTENDED FOR COMMERCIAL PURPOSES
+#    AND SHOULD BE USED ONLY FOR NON-COMMERCIAL RESEARCH PURPOSES.  THE
+#    SOFTWARE MAY NOT IN ANY EVENT BE USED FOR ANY CLINICAL OR DIAGNOSTIC
+#    PURPOSES.  YOU ACKNOWLEDGE AND AGREE THAT THE SOFTWARE IS NOT INTENDED FOR
+#    USE IN ANY HIGH RISK OR STRICT LIABILITY ACTIVITY, INCLUDING BUT NOT
+#    LIMITED TO LIFE SUPPORT OR EMERGENCY MEDICAL OPERATIONS OR USES.  LICENSOR
+#    MAKES NO WARRANTY AND HAS NOR LIABILITY ARISING FROM ANY USE OF THE
+#    SOFTWARE IN ANY HIGH RISK OR STRICT LIABILITY ACTIVITIES.
+
+# Brief: Update utility.
+
+import sys
+import os
+from PyQt4 import QtGui, QtCore
+
+def update():
+    
+    app = QtGui.QApplication(sys.argv)
+
+    w = QtGui.QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('GPI Update')
+    w.show()
+    
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    update()
