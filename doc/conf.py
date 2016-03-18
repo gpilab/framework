cd34aa173a5d8da97f89067645b2fdb16f201267
ash <aganders3@gmail.com>
2016-03-18 13:30:14 -0700

    Remove member skipping code for now, just use default rules.

--- doc/conf.py
+++ doc/conf.py
@@ -293,10 +293,10 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
-def skip_member(app, what, name, obj, skip, options):
-    print app, what, name, obj, skip, options
-    return False
-
-def setup(app):
-    app.connect('autodoc-skip-member', skip_member)
+# def skip_member(app, what, name, obj, skip, options):
+#     print(app, what, name, obj, skip, options)
+#     return False
+#
+# def setup(app):
+#     app.connect('autodoc-skip-member', skip_member)
 
