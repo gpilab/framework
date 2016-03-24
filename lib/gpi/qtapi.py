f81565181ec80a0ccdbdc66121cf85c37c5854e0
nick <nick.zwart@gmail.com>
2016-03-24 16:03:39 -0700

    Reverted to eeb657e9253cbce.

--- lib/gpi/qtapi.py
+++ lib/gpi/qtapi.py
@@ -24,79 +24,30 @@
 
 # Brief: This setup can be used to force the APIv2 for future compatibility.
 
-import os
 
 from .logger import manager
 # start logger for this module
 log = manager.getLogger(__name__)
 
+import sip
+# To determine which API's to set:
+#   http://pyqt.sourceforge.net/Docs/PyQt4/incompatible_apis.html
+_APIv2 = True
+if _APIv2:
+    sip.setapi('QDate', 2)
+    sip.setapi('QDateTime', 2)
+    sip.setapi('QString', 2)
+    sip.setapi('QTextStream', 2)
+    sip.setapi('QTime', 2)
+    sip.setapi('QUrl', 2)
+    sip.setapi('QVariant', 2)
+import PyQt4.QtCore as _QtCore
+QtCore = _QtCore
+
 def import_module(moduleName):
     p = __import__('PyQt4', globals(), locals(), [moduleName], 0)
     return getattr(p, moduleName)
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if not on_rtd:
-    import sip
-    # To determine which API's to set:
-    #   http://pyqt.sourceforge.net/Docs/PyQt4/incompatible_apis.html
-    _APIv2 = True
-    if _APIv2:
-        sip.setapi('QDate', 2)
-        sip.setapi('QDateTime', 2)
-        sip.setapi('QString', 2)
-        sip.setapi('QTextStream', 2)
-        sip.setapi('QTime', 2)
-        sip.setapi('QUrl', 2)
-        sip.setapi('QVariant', 2)
-
-    # stub in PyQt4 imports
-    import PyQt4.QtCore as _QtCore
-    QtCore = _QtCore
-
-    Signal = QtCore.pyqtSignal
-    Slot = QtCore.pyqtSlot
-    Property = QtCore.pyqtProperty
-
-else: # READTHEDOCS
-
-    # mock Qt when we are generating documentation at readthedocs.org
-    class Mock(object):
-        def __init__(self, *args, **kwargs):
-            pass
-    
-        def __call__(self, *args, **kwargs):
-            return Mock()
-    
-        @classmethod
-        def __getattr__(cls, name):
-            if name in ('__file__', '__path__'):
-                return '/dev/null'
-            elif name in ('__name__', '__qualname__'):
-                return name
-            elif name == '__annotations__':
-                return {}
-            else:
-                return Mock()
-    
-    QtGui = Mock()
-    QtCore = Mock()
-    QtTest = Mock()
-    Qt = Mock()
-    QEvent = Mock()
-    QApplication = Mock()
-    QWidget = Mock()
-    qInstallMsgHandler = Mock()
-    qInstallMessageHandler = Mock()
-    qDebug = Mock()
-    qWarning = Mock()
-    qCritical = Mock()
-    qFatal = Mock()
-    QtDebugMsg = Mock()
-    QtWarningMsg = Mock()
-    QtCriticalMsg = Mock()
-    QtFatalMsg = Mock()
-    QT_API = '<none>' 
-
-    Signal = Mock()
-    Slot = Mock()
-    Property = Mock()
+Signal = QtCore.pyqtSignal
+Slot = QtCore.pyqtSlot
+Property = QtCore.pyqtProperty
