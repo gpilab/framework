4e65f7ae0563b800e1c0d871d6f9c46a98e53451
nick <nick.zwart@gmail.com>
2016-03-25 13:40:29 -0700

    Reverted back to bootstrap since py3doc is broken for mobile.

--- doc/conf.py
+++ doc/conf.py
@@ -16,6 +16,7 @@
 import sys
 import os
 import subprocess
+import sphinx_bootstrap_theme
 import sphinx_py3doc_enhanced_theme
 
 os.system('pip list')
@@ -130,8 +131,76 @@ todo_include_todos = False
 
 # -- Options for HTML output ----------------------------------------------
 # sphinx_bootstrap_theme
-html_theme = "sphinx_py3doc_enhanced_theme"
-html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+#html_logo = 'logo.png'
+html_theme_options = {
+    # Navigation bar title. (Default: ``project`` value)
+    'navbar_title': "GPI Docs",
+
+    # Tab name for entire site. (Default: "Site")
+    'navbar_site_name': "Contents",
+
+    # A list of tuples containing pages or urls to link to.
+    # Valid tuples should be in the following forms:
+    #    (name, page)                 # a link to a page
+    #    (name, "/aa/bb", 1)          # a link to an arbitrary relative url
+    #    (name, "http://example.com", True) # arbitrary absolute url
+    # Note the "1" or "True" value above as the third argument to indicate
+    # an arbitrary url.
+    #'navbar_links': [
+    #    ("Examples", "examples"),
+    # #   ("Link", "http://example.com", True),
+    #],
+
+    # Render the next and previous page links in navbar. (Default: true)
+    'navbar_sidebarrel': False,
+
+    # Render the current pages TOC in the navbar. (Default: true)
+    'navbar_pagenav': True,
+
+    # Tab name for the current pages TOC. (Default: "Page")
+    'navbar_pagenav_name': "This Page",
+
+    # Global TOC depth for "site" navbar tab. (Default: 1)
+    # Switching to -1 shows all levels.
+    'globaltoc_depth': 2,
+
+    # Include hidden TOCs in Site navbar?
+    #
+    # Note: If this is "false", you cannot have mixed ``:hidden:`` and
+    # non-hidden ``toctree`` directives in the same page, or else the build
+    # will break.
+    #
+    # Values: "true" (default) or "false"
+    'globaltoc_includehidden': "true",
+
+    # HTML navbar class (Default: "navbar") to attach to <div> element.
+    # For black navbar, do "navbar navbar-inverse"
+    'navbar_class': "navbar navbar-inverse",
+
+    # Fix navigation bar to top of page?
+    # Values: "true" (default) or "false"
+    'navbar_fixed_top': "true",
+
+    # Location of link to source.
+    # Options are "nav" (default), "footer" or anything else to exclude.
+    'source_link_position': "footer",
+
+    # Bootswatch (http://bootswatch.com/) theme.
+    #
+    # Options are nothing (default) or the name of a valid theme
+    # such as "cosmo", "lumen", "paper".
+    'bootswatch_theme': "lumen",
+
+    # Choose Bootstrap version.
+    # Values: "3" (default) or "2" (in quotes)
+    'bootstrap_version': "3",
+}
+
+# sphinx_py3doc_enhanced_theme
+#html_theme = "sphinx_py3doc_enhanced_theme"
+#html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
 #html_theme_options = {
 #   'githuburl': 'https://github.com/gpilab/framework',
 #   'bodyfont': '"Lucida Grande",Arial,sans-serif',
