5879f1356f4cfe5c54390337c6b8f763a29fc948
Daniel Borup <borup.daniel@mayo.edu>
2020-01-07 15:38:55 -0600

    Add batch file for launching with Windows shells

---
+++ launch/gpi_cmd.bat
@@ -0,0 +1,38 @@
+@echo off
+
+::    Copyright (C) 2014  Dignity Health
+::
+::    This program is free software: you can redistribute it and/or modify
+::    it under the terms of the GNU Lesser General Public License as published by
+::    the Free Software Foundation, either version 3 of the License, or
+::    (at your option) any later version.
+::
+::    This program is distributed in the hope that it will be useful,
+::    but WITHOUT ANY WARRANTY; without even the implied warranty of
+::    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+::    GNU Lesser General Public License for more details.
+::
+::    You should have received a copy of the GNU Lesser General Public License
+::    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+::
+::    NO CLINICAL USE.  THE SOFTWARE IS NOT INTENDED FOR COMMERCIAL PURPOSES
+::    AND SHOULD BE USED ONLY FOR NON-COMMERCIAL RESEARCH PURPOSES.  THE
+::    SOFTWARE MAY NOT IN ANY EVENT BE USED FOR ANY CLINICAL OR DIAGNOSTIC
+::    PURPOSES.  YOU ACKNOWLEDGE AND AGREE THAT THE SOFTWARE IS NOT INTENDED FOR
+::    USE IN ANY HIGH RISK OR STRICT LIABILITY ACTIVITY, INCLUDING BUT NOT
+::    LIMITED TO LIFE SUPPORT OR EMERGENCY MEDICAL OPERATIONS OR USES.  LICENSOR
+::    MAKES NO WARRANTY AND HAS NO LIABILITY ARISING FROM ANY USE OF THE
+::    SOFTWARE IN ANY HIGH RISK OR STRICT LIABILITY ACTIVITIES.
+
+:: The GPI launcher script.  The .command suffix is used by OSX open.
+
+:: get user environment settings
+::   -this will pickup the user's visual editor
+
+set ANACONDA=/opt/anaconda1anaconda2anaconda3
+set PYTHON=%ANACONDA%\python
+set GPI_LAUNCH=%ANACONDA%\Scripts\gpi_launch
+
+%PYTHON% %GPI_LAUNCH% -style Windows %*
+
+
